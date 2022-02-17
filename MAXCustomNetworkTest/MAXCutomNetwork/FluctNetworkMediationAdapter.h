4bbdae068774faa99d43307c53aa6f329a12a3f1
saitoiotken <saitoiotken@users.noreply.github.com>
2022-02-17 16:28:15 +0900

    ...

---
+++ MAXCustomNetworkTest/MAXCutomNetwork/FluctNetworkMediationAdapter.h
@@ -0,0 +1,17 @@
+//
+//  FluctNetworkMediationAdapter.h
+//  MAXCustomNetworkTest
+//
+//  Created by y-saito21 on 2022/02/17.
+//
+
+#import <AppLovinSDK/AppLovinSDK.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FluctNetworkMediationAdapter : ALMediationAdapter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MAXCustomNetworkTest/MAXCutomNetwork/FluctNetworkMediationAdapter.m
@@ -0,0 +1,25 @@
+//
+//  FluctNetworkMediationAdapter.m
+//  MAXCustomNetworkTest
+//
+//  Created by y-saito21 on 2022/02/17.
+//
+
+#import "FluctNetworkMediationAdapter.h"
+
+@implementation FluctNetworkMediationAdapter
+
+- (void)initializeWithParameters:(id<MAAdapterInitializationParameters>)parameters completionHandler:(void (^)(MAAdapterInitializationStatus initializationStatus, NSString *_Nullable errorMessage))completionHandler {
+    
+    completionHandler(MAAdapterInitializationStatusInitializedSuccess, nil);
+}
+
+- (NSString *)SDKVersion {
+    return @"1.0.0.0";
+}
+
+- (NSString *)adapterVersion {
+    return @"1.0.0.0.0";
+}
+
+@end
